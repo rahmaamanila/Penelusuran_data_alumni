a4adb40a796e2d7e530046ce678130723e7798f1
rahmaamanila <rahmaamanila@gmail.com>
2022-10-08 15:39:21 +0700

    Edit teks di halaman edit

--- resources/views/alumni/editt.blade.php
+++ resources/views/alumni/editt.blade.php
@@ -26,7 +26,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Edit Data Alumni</h1>
+            <h1 class="m-0">Edit Data Alumni rahma</h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <!-- <ol class="breadcrumb float-sm-right">
