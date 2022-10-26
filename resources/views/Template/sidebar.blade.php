7fb70b62d31fdfa1cdd4f518c6653cc40d68b146
rahmaamanila <rahmaamanila@gmail.com>
2022-10-26 13:39:40 +0700

    ubah

--- resources/views/Template/sidebar.blade.php
+++ resources/views/Template/sidebar.blade.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-light-info elevation-4">
+<aside class="main-sidebar sidebar-light-info">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
       <img src="{{asset('Gambar/logo.png')}}" alt="Admin Logo" class="brand-image img-circle" style="opacity: .8">
--- resources/views/alumni/index.blade.php
+++ resources/views/alumni/index.blade.php
@@ -77,7 +77,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         <thead>
                             <tr class="text-center table-info">
                                 <th>No</th>
-                                <!-- <th>Foto</th> -->
                                 <th>Nik</th>
                                 <th>Nama</th>
                                 <th>Jenis Kelamin</th>
@@ -91,9 +90,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
                             @foreach($alumni as $i => $p)
                             <tr class="text-center">
                                 <td>{{ $i+1 }}</td>
-                                <!-- <td>
-                                  <img src="{{ asset('img/'. $p->foto ) }}" height="10%" width="20%" alt="">
-                                </td> -->
                                 <td>{{ $p->nik }}</td>
                                 <td>{{ $p->nama }}</td>
                                 <td>{{ $p->jenkel }}</td>
